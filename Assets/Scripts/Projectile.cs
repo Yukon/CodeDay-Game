a4386393c72e16051d44f9f8638f0bf9bea7644f
Yukon Vinecki <yukonvinecki@gmail.com>
2015-02-15 00:09:51 -0800

    Destroy projectile when exiting view

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -14,6 +14,13 @@ public class Projectile : MonoBehaviour {
 		rigidbody2D.AddForce(transform.up * speed);
 	}
 
+	void Update() {
+		Vector3 cameraView = Camera.main.WorldToViewportPoint(transform.position);
+		if (cameraView.x > 1 || cameraView.x < 0 || cameraView.y > 1 || cameraView.y < 0) {
+			Destroy(this.gameObject);
+		}
+	}
+
 	void OnCollisionEnter2D(Collision2D collision) {
 		if (collision.gameObject.tag != "Player") {
 			Destroy(this.gameObject);
